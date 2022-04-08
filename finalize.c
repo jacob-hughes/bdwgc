ec80f63d49787c2dd1e4ce07bdccccfa2a8d22be
Jake Hughes <jh@jakehughes.uk>
2022-04-08 17:23:31 +0100

    Remove assertions

--- finalize.c
+++ finalize.c
@@ -999,9 +999,6 @@ GC_INNER void GC_finalize(void)
         GC_ASSERT(GC_size(curr_fo) >= sizeof(struct finalizable_object));
         real_ptr = (ptr_t)GC_REVEAL_POINTER(curr_fo->fo_hidden_base);
         if (!GC_is_marked(real_ptr) && GC_is_managed(real_ptr)) {
-            struct hblk *h = HBLKPTR(real_ptr);
-            hdr * hhdr = HDR(h);
-            assert(!IS_UNCOLLECTABLE(hhdr->hb_obj_kind));
             GC_MARKED_FOR_FINALIZATION(real_ptr);
             GC_MARK_FO(real_ptr, curr_fo -> fo_mark_proc);
             if (GC_is_managed_marked(real_ptr)) {
@@ -1019,9 +1016,6 @@ GC_INNER void GC_finalize(void)
       while (curr_fo != 0) {
         real_ptr = (ptr_t)GC_REVEAL_POINTER(curr_fo->fo_hidden_base);
         if (!GC_is_marked(real_ptr) && GC_is_managed(real_ptr)) {
-            struct hblk *h = HBLKPTR(real_ptr);
-            hdr * hhdr = HDR(h);
-            assert(!IS_UNCOLLECTABLE(hhdr->hb_obj_kind));
             if (!GC_java_finalization) {
               GC_set_mark_bit(real_ptr);
             }
